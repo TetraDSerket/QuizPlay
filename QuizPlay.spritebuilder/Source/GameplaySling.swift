d2e4b88b1e45aeb333485162d10bcc5dc29450da
Varsha Ramakrishnan <varsha.r22@gmail.com>
2015-07-14 14:20:13 -0700

    sling working and able to launch things

--- QuizPlay.spritebuilder/Source/GameplaySling.swift
+++ QuizPlay.spritebuilder/Source/GameplaySling.swift
@@ -8,23 +8,73 @@
 
 import UIKit
 
-class GameplaySling: CCNode
+class GameplaySling: CCNode, CCPhysicsCollisionDelegate
 {
     weak var gamePhysicsNode: CCPhysicsNode!
-    weak var slingPocket: CCSprite!
+    weak var slingPocket: CCNode!
+    weak var contentNode: CCNode!
+    weak var mouseJointNode: CCNode!
+    weak var hero: CCSprite!
+    var mouseJoint: CCPhysicsJoint?
+    var actionFollow: CCActionFollow?
     var quizWords = Dictionary<String, String>()
+    
+    func didLoadFromCCB()
+    {
+        userInteractionEnabled = true
+        gamePhysicsNode.collisionDelegate = self
+        mouseJointNode.physicsBody.collisionMask = []
+
+    }
     
     override func onEnterTransitionDidFinish()
     {
-//        let actionFollow = CCActionFollow(target: slingPocket, worldBoundary: boundingBox())
-//        gamePhysicsNode.runAction(actionFollow)
+        let actionFollow = CCActionFollow(target: hero, worldBoundary: boundingBox())
+        contentNode.runAction(actionFollow)
         gamePhysicsNode.debugDraw = true
+    }
+    
+    override func update(delta: CCTime)
+    {
+        slingPocket.physicsBody.velocity.x = 0
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!)
     {
-        //let touchLocation = touch.locationInNode(contentNode)
-        
+        let touchLocation = touch.locationInNode(contentNode)
+        if CGRectContainsPoint(slingPocket.boundingBox(), touchLocation)
+        {
+            mouseJointNode.position = touchLocation
+            mouseJoint = CCPhysicsJoint.connectedSpringJointWithBodyA(mouseJointNode.physicsBody, bodyB: slingPocket.physicsBody, anchorA: CGPointZero, anchorB: CGPoint(x: 150, y: 150), restLength: 0, stiffness: 3000, damping: 150)
+            
+        }
+    }
+    
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!)
+    {
+        // whenever touches move, update the position of the mouseJointNode to the touch position
+        let touchLocation = touch.locationInNode(contentNode)
+        mouseJointNode.position = touchLocation
+    }
+    
+    func releaseCatapult()
+    {
+        if let joint = mouseJoint
+        {
+            // releases the joint and lets the catapult snap back
+            joint.invalidate()
+            mouseJoint = nil
+        }
+    }
+    
+    override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!)
+    {
+        releaseCatapult()
+    }
+    
+    override func touchCancelled(touch: CCTouch!, withEvent event: CCTouchEvent!)
+    {
+        releaseCatapult()
     }
     
     func initializeQuizWordsArray()
