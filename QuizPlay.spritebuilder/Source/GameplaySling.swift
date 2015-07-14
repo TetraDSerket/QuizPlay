b9559bc8c01292fe2c3ce4b2bc06d82bcca712db
Varsha Ramakrishnan <varsha.r22@gmail.com>
2015-07-14 13:18:47 -0700

    Got the sling thing working pretty well i think

--- QuizPlay.spritebuilder/Source/GameplaySling.swift
+++ QuizPlay.spritebuilder/Source/GameplaySling.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
-class GameplaySling: CCNode {
+class GameplaySling: CCNode
+{
+    weak var gamePhysicsNode: CCPhysicsNode!
+    weak var slingPocket: CCSprite!
+    var quizWords = Dictionary<String, String>()
+    
+    override func onEnterTransitionDidFinish()
+    {
+//        let actionFollow = CCActionFollow(target: slingPocket, worldBoundary: boundingBox())
+//        gamePhysicsNode.runAction(actionFollow)
+        gamePhysicsNode.debugDraw = true
+        
+    }
+    
+    func initializeQuizWordsArray()
+    {
+        quizWords["Vriska"] = "There are so many irons in the fire that the fire has retired"
+        quizWords["Robin"] = "Oh they've probably been shot to pieces and maimed"
+        quizWords["Keladry"] = "Be like stone. Pretty and smooth, but able to bash someone's head in"
+        quizWords["Tetra"] = "Being a pirate is tough, especially when people don't want you to steal their things"
+        quizWords["Inkling"] = "Identity crisis"
+    }
    
+    
 }
