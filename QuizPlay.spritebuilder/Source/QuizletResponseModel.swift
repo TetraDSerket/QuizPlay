a5edc0da2b1844e620c0cc03c4625c1a33edd08b
Varsha Ramakrishnan <varsha.r22@gmail.com>
2015-07-17 14:22:05 -0700

    with alamofire

---
+++ QuizPlay.spritebuilder/Source/QuizletResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  QuizletResponseModel.swift
+//  QuizPlay
+//
+//  Created by Varsha Ramakrishnan on 7/16/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class QuizletResponseModel: NSObject {
+   
+}
